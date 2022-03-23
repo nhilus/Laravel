a2afaf61dbc7b057ade463856b696cf498c839b1
António Barbosa <tobarbosa11@gmail.com>
2022-03-23 23:14:00 +0000

    lorem to cards

--- resources/views/components/cards/cards.blade.php
+++ resources/views/components/cards/cards.blade.php
@@ -4,8 +4,8 @@
             @component('components.card.card',
                 [
                 'image' => 'https://www.audi.pt/media/Theme_Menu_Model_Dropdown_Item_Image_Component/root-pt-model-modelMenu-editableItems-72736-dropdown-476668-image/dh-487-a0e9a6/770b761f/1635486188/audi-a3-limousine-tfsi.jpg',
-                'title' => 'Title 100',
-                'description' => 'Desc 1',
+                'title' => 'Here',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut aliquam purus sit amet luctus venenatis lectus. Diam vulputate ut pharetra sit amet. Tellus elementum sagittis vitae et. Nam at lectus urna duis convallis convallis tellus id interdum. Placerat duis ultricies lacus sed turpis. Iaculis urna id volutpat lacus laoreet non curabitur gravida arcu.',
                 ]
                 )
             @endcomponent
@@ -14,8 +14,8 @@
             @component('components.card.card',
                 [
                 'image' => 'https://www.audi.pt/media/Theme_Menu_Model_Dropdown_Item_Image_Component/root-pt-model-modelMenu-editableItems-73240-dropdown-476801-image/dh-500-a0e9a6/3203b915/1635486191/audi-a7-sportback-tfsi-e-frontansicht.jpg',
-                'title' => 'Title 2',
-                'description' => 'Desc 2',
+                'title' => 'There',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut aliquam purus sit amet luctus venenatis lectus. Diam vulputate ut pharetra sit amet. Tellus elementum sagittis vitae et. Nam at lectus urna duis convallis convallis tellus id interdum. Placerat duis ultricies lacus sed turpis. Iaculis urna id volutpat lacus laoreet non curabitur gravida arcu.',
                 ]
                 )
             @endcomponent
@@ -24,8 +24,8 @@
             @component('components.card.card',
                 [
                 'image' => 'https://www.audi.pt/media/Theme_Menu_Model_Dropdown_Item_Image_Component/root-pt-model-modelMenu-editableItems-73404-dropdown-476862-image/dh-487-a0e9a6/96de6158/1635486193/1277x718-audi-q3-sportback-tfsi-e-rear-q3-2020-1408-oe.jpg',
-                'title' => 'Title 3',
-                'description' => 'Desc 3',
+                'title' => 'and Gone',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam at lectus urna duis convallis convallis tellus id interdum. Placerat duis ultricies lacus sed turpis. Iaculis urna id volutpat lacus laoreet non curabitur gravida arcu.',
                 ]
                 )
             @endcomponent
